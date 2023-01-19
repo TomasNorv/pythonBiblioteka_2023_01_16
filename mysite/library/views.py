304eb7bb1ca81fdc872c517ff1888464c3217949
TomasNorvilis <tomasnorvilis@yahoo.com>
2023-01-19 14:52:22 +0200

    padariau puslapi su knygom, autoriais,egzemplioriais, statusu

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, BookInstance, Author
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    num_books = Book.objects.all().count()       # Suskaičiuokime keletą pagrindinių objektų
+    num_instances = BookInstance.objects.all().count() # Suskaičiuokime keletą pagrindinių objektų
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count() # Laisvos knygos (tos, kurios turi statusą 'g')
+    num_authors = Author.objects.count()    # Kiek yra autorių
+
+    # perduodame informaciją į šabloną žodyno(dictionary) pavidale:
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # renderiname index.html, su duomenimis kintamąjame context
+    return render(request, 'index.html', context = context)
